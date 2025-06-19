2384675cc0803fbf7994969c0bcaf5318ba3bc82
sathya <sathyaqueen2025@gmail.com>
2025-06-19 13:25:46 +0530

    Create integer.c

---
+++ day1/integer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int number;
+    {
+    printf("Enter the number: ");
+    scanf("%d", &number);
+}
+    if (number>0) 
+    {
+        printf("positive");
+    } 
+    else if (number<0) 
+    {
+        printf("negative");
+    } 
+    else {
+        printf("zero");
+    }
+
+     return 0;
+}
