35f1dc6f89db37e7c44fd93f45856f882b6f3721
sathya <sathyaqueen2025@gmail.com>
2025-06-23 09:25:58 +0530

    Create numberscount.c

---
+++ day9/numberscount.c
@@ -0,0 +1,22 @@
+def look_and_say(n):
+    term = "1"
+    print(term)  # Print the first term
+
+    for _ in range(1, n):
+        next_term = ""
+        count = 1
+
+        for j in range(1, len(term) + 1):
+            if j < len(term) and term[j] == term[j - 1]:
+                count += 1
+            else:
+                next_term += str(count) + term[j - 1]
+                count = 1
+
+        print(next_term)
+        term = next_term  # Update current term
+
+
+# Input from user
+n = int(input("Enter number of terms: "))
+look_and_say(n)
